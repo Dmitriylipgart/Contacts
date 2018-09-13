3da94d3902bbe075fca38c1ddf06f5be94611d59
Student <dmitriylipgart@gmail.com>
2018-09-13 03:20:57 -0700

    fix try connection

--- model/src/main/java/dao/DatabaseService.java
+++ model/src/main/java/dao/DatabaseService.java
@@ -8,46 +8,43 @@ import java.sql.*;
 
 public class DatabaseService {
 
-    private final String URL = "jdbc:mysql://localhost:3306/contactlist?autoReconnect=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final String URL = "jdbc:mysql://localhost:3306/contact_list";
     private final String USER = "root";
     private final String PASSWORD = "root";
 
 
     public Connection getConnection() {
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+//        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+//        }
 
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)){
-            return connection;
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
         return null;
     }
 
-    public void createConnection(){
-
-    }
 
     public void createContact(Contact contact){
 
 
-        String sql = "Insert into contacts (firstName, lastName, middleName)"
+        String sql = "Insert into contacts (first_name, last_name, middle_name)"
                 + " values (?,?,?)";
         try {
             Connection connection = getConnection();
             PreparedStatement statement = connection.prepareStatement(sql);
-            System.out.println(statement);
-            System.out.println(connection);
             statement.setString(1, contact.getFirstName());
             statement.setString(2, contact.getLastName());
             statement.setString(3, contact.getMiddleName());
             statement.executeUpdate();
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
